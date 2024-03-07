93d3bb16b99e0e26ef4f173be669bbb1620ec782
jigar333 <jmvarmora@gmail.com>
2024-03-07 11:58:26 +0530

    production API_URL update

--- utils/constant.js
+++ utils/constant.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5173"
-    : "https://attractive-erin-termite.cyclic.app";
+    : "https://stock.iqinfy.com";
