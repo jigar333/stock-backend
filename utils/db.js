be5d72bad134c8f1d4566a147ed35f2a6ca427d6
jigar333 <jmvarmora@gmail.com>
2024-03-19 11:42:54 +0530

    env file update

--- utils/db.js
+++ utils/db.js
@@ -1,23 +1,14 @@
 import mysql from "mysql";
 let mysqlConfig;
-// Check if running locally or on live server
-if (process.env.NODE_ENV === "production") {
-  // Load live server configurations
-  mysqlConfig = {
-    host: process.env.LIVE_MYSQL_HOST,
-    user: process.env.LIVE_MYSQL_USER,
-    password: process.env.LIVE_MYSQL_PASSWORD,
-    database: process.env.LIVE_MYSQL_DATABASE,
-  };
-} else {
-  // Load local configurations
-  mysqlConfig = {
-    host: process.env.LOCAL_MYSQL_HOST,
-    user: process.env.LOCAL_MYSQL_USER,
-    password: process.env.LOCAL_MYSQL_PASSWORD,
-    database: process.env.LOCAL_MYSQL_DATABASE,
-  };
-}
+
+// Load server configurations
+mysqlConfig = {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+};
+
 const con = mysql.createConnection(mysqlConfig);
 con.connect(function (err) {
   console.log(mysqlConfig);
